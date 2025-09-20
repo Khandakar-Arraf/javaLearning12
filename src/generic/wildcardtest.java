f7e847ee3f692f2b4b608afaf86cbce6d937dfea
Khandaker Arraf <52911060+Khandakar-Arraf@users.noreply.github.com>
2025-09-20 19:27:00 +0600

    Delete src/generic directory

--- src/generic/wildcardtest.java
+++
@@ -1,8 +0,0 @@
-package generic;
-
-public class wildcardtest {
-    public static void main(String[] args) {
-        wildcard <Integer> num = new wildcard<>(33);
-        System.out.println(num.getvalue());
-    }
-}
